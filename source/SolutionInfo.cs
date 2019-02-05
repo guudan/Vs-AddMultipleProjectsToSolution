01fb88cebbf981144da2768f138e81827ebb61aa
Maciej Gudanowicz <mgudan.github@gmail.com>
2019-02-05 20:30:11 +0100

    Configure build and test using cake.

---
+++ source/SolutionInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Add Multiple Projects To Solution")]
+[assembly: AssemblyCopyright("2019 Maciej Gudanowicz")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
